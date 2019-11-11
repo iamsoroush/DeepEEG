3f8e9fb5b923365ae2b985bbbcce2a3064ca59d1
Soroush Moazed <33500173+iamsoroush@users.noreply.github.com>
2019-11-11 20:59:08 +0330

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-tf_version = tf.__version__
-print('tensorflow version: ', tf_version)
-
-if tf.__version__.startswith('2'):
-    from tensorflow import keras
-else:
-    import keras
